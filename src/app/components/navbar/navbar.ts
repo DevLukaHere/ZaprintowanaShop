b7b3a279751896c055ab3929a07f58c0335a5bed
Łukasz Sobieski <luk.jag@o2.pl>
2026-07-22 23:27:57 +0200

    Added responsiveness for mobiles

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { COLLECTIONS, Collection } from '../../models/collection';
 import { CartService } from '../../services/cart.service';
 
@@ -10,6 +10,26 @@ import { CartService } from '../../services/cart.service';
 })
 export class Navbar {
   protected readonly cart = inject(CartService);
+  protected readonly mobileMenuOpen = signal(false);
+  protected readonly panelOpen = signal(false);
+  protected readonly panelContent = signal<'wishlist' | 'cart'>('wishlist');
+
+  protected toggleMobileMenu(): void {
+    this.mobileMenuOpen.update((open) => !open);
+  }
+
+  protected openPanel(type: 'wishlist' | 'cart'): void {
+    if (this.panelOpen() && this.panelContent() === type) {
+      this.panelOpen.set(false);
+      return;
+    }
+    this.panelContent.set(type);
+    this.panelOpen.set(true);
+  }
+
+  protected closePanel(): void {
+    this.panelOpen.set(false);
+  }
 
   protected getCollection(id: string): Collection | undefined {
     return COLLECTIONS.find((collection) => collection.id === id);
